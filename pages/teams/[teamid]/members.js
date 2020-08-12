2f73378cc6fc65007dfafde908b817ee39a8a7a2
JamesJ <1869237+shinygit@users.noreply.github.com>
2020-08-12 18:25:29 -0500

    alter role button

--- pages/teams/[teamid]/members.js
+++ pages/teams/[teamid]/members.js
@@ -1,13 +1,19 @@
-import { createJoinCode } from '../../../api/mutations'
-import useSWR from 'swr'
+import { createJoinCode, setRole } from '../../../api/mutations'
+import useSWR, { mutate } from 'swr'
+import { useSession } from 'next-auth/client'
 
 import { useRouter } from 'next/router'
 const TeamMembers = () => {
+  const [session, loading] = useSession()
   const router = useRouter()
   const { teamid } = router.query
-  const { data: teamMembers = [], error: teamMembersError } = useSWR(
+
+  const { data: teamMembers, error: teamMembersError } = useSWR(
     teamid ? `/api/teams/${teamid}/members` : null
   )
+  if (!teamMembers) return <div>Loading...</div>
+  const isOwner =
+    teamMembers.find((member) => member.id === session.userId).role === 'OWNER'
   const handleJoinCodeClick = async () => {
     const teamWithJoinCode = await createJoinCode(teamid)
     alert(
@@ -15,12 +21,34 @@ const TeamMembers = () => {
       This code is one time use and expires in 5 minutes.`
     )
   }
+  const handleAlterRole = async (id, currentRole) => {
+    const newRole = await setRole(teamid, {
+      userId: id,
+      role: currentRole === 'USER' ? 'SHARED' : 'USER',
+    })
+    if (newRole) mutate(`/api/teams/${teamid}/members`)
+  }
+  const roles = {
+    OWNER: 'Owner',
+    USER: 'User',
+    SHARED: 'Shared',
+  }
   return (
     <div>
       <button onClick={handleJoinCodeClick}>Create Join Code</button>
       Members:
       {teamMembers.map((member) => (
-        <div key={member.id}>{member.name}</div>
+        <div key={member.id}>
+          {member.name} - {roles[member.role]}{' '}
+          {isOwner && (
+            <button
+              onClick={() => handleAlterRole(member.id, member.role)}
+              className='btn btn-white'
+            >
+              Alter Role
+            </button>
+          )}
+        </div>
       ))}
     </div>
   )
