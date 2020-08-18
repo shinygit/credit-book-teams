7127c888b9e4d38f6a069787255cbd5543229f60
JamesJ <1869237+shinygit@users.noreply.github.com>
2020-08-17 20:20:22 -0500

    claim credit confirmation

--- components/credits/credit_record/ClaimCredit.js
+++ components/credits/credit_record/ClaimCredit.js
@@ -2,11 +2,27 @@ import { mutate, cache } from 'swr'
 import { useRouter } from 'next/router'
 import { claimCredit } from '../../../api/mutations'
 
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Button,
+} from '@chakra-ui/core'
+
 const ClaimCredit = ({ credit }) => {
+  const [isOpen, setIsOpen] = React.useState()
+  const onClose = () => setIsOpen(false)
+  const cancelRef = React.useRef()
+
   const router = useRouter()
   const { teamid } = router.query
-
-  const handleClick = async () => {
+  const handleClick = () => {
+    setIsOpen(true)
+  }
+  const handleClaim = async () => {
     const claimedCredit = await claimCredit(teamid, credit.id)
     cache
       .keys()
@@ -49,22 +65,54 @@ const ClaimCredit = ({ credit }) => {
       </svg>
     )
   return (
-    <button onClick={handleClick}>
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        width='44'
-        height='44'
-        viewBox='0 0 24 24'
-        strokeWidth='1.5'
-        stroke='#2c3e50'
-        fill='none'
-        strokeLinecap='round'
-        strokeLinejoin='round'
+    <>
+      <button onClick={handleClick}>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='44'
+          height='44'
+          viewBox='0 0 24 24'
+          strokeWidth='1.5'
+          stroke='#2c3e50'
+          fill='none'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        >
+          <path stroke='none' d='M0 0h24v24H0z' />
+          <rect x='4' y='4' width='16' height='16' rx='2' />
+        </svg>
+      </button>
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
       >
-        <path stroke='none' d='M0 0h24v24H0z' />
-        <rect x='4' y='4' width='16' height='16' rx='2' />
-      </svg>
-    </button>
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+              Claim Credit
+            </AlertDialogHeader>
+            <AlertDialogBody>
+              Are you sure? You can't undo this action afterwards.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <button
+                className='btn btn-white'
+                ref={cancelRef}
+                onClick={onClose}
+              >
+                Cancel
+              </button>
+              <button className='ml-4 bg-gray-300 btn' onClick={onClose}>
+                Claim
+              </button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   )
 }
 
