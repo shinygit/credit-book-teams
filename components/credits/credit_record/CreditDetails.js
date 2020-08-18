4cc907f59e8d16bd889c26ecccc8b08ef9df85ca
JamesJ <1869237+shinygit@users.noreply.github.com>
2020-08-17 20:20:11 -0500

    credit details show if manually typed creator

--- components/credits/credit_record/CreditDetails.js
+++ components/credits/credit_record/CreditDetails.js
@@ -2,12 +2,17 @@ import ClaimCredit from './ClaimCredit'
 import dayjs from 'dayjs'
 import { formatPhoneNumber } from '../../../utils/formatPhoneNumber'
 const CreditDetails = ({ credit }) => {
+  console.log(credit)
   return (
     <div className='flex justify-between p-2 mb-3 bg-white border-2 border-black rounded'>
       <div className='flex flex-col'>
         <div className='flex'>
           {dayjs(credit.createdAt).format('MM/DD/YY')}
-          <span className='pl-2'>{credit.createdBy?.name}</span>
+          <span className={`pl-2`}>
+            {credit.writtenOnSharedBy
+              ? `Typed: ${credit.writtenOnSharedBy}`
+              : credit.createdBy?.name}
+          </span>
         </div>
         <div className='flex flex-col'>
           <span className='text-3xl'>{credit.name}</span>
