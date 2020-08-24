a783441d07cdc76832f576eb1711e13638571f4b
JamesJ <1869237+shinygit@users.noreply.github.com>
2020-08-24 14:08:02 -0500

    fix for single element return

--- components/credits/ListCredits.js
+++ components/credits/ListCredits.js
@@ -1,7 +1,11 @@
 import CreditDetails from './credit_record/CreditDetails'
 const ListCredits = ({ credits = [] }) => {
-  return credits.map((credit) => (
-    <CreditDetails credit={credit} key={credit.id} />
-  ))
+  return (
+    <>
+      {credits.map((credit) => (
+        <CreditDetails credit={credit} key={credit.id} />
+      ))}
+    </>
+  )
 }
 export default ListCredits
