2d1629441e7e84f3e954d2e04c2c5d76923e1284
JamesJ <1869237+shinygit@users.noreply.github.com>
2020-08-24 14:09:20 -0500

    changed to ts

--- middleware/addUserIdToReq.js
+++
@@ -1,7 +0,0 @@
-import { getSession } from 'next-auth/client'
-const addUserIdToReq = async (req, res) => {
-  const { userId } = (await getSession({ req })) || {}
-  // if (!userId) return res.status(401).json({ error: 'Not logged in.' })
-  req.userId = userId
-}
-export { addUserIdToReq }
